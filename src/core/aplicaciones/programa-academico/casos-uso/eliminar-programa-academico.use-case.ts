d2718a4958b1015c9402e26d5109b68456f92387
Deilons <apolo4748@gmail.com>
2025-11-19 22:57:22 -0500

    feat: implementacion de errores en el caso de uso de programa academico

--- src/core/aplicaciones/programa-academico/casos-uso/eliminar-programa-academico.use-case.ts
+++ src/core/aplicaciones/programa-academico/casos-uso/eliminar-programa-academico.use-case.ts
@@ -1,19 +1,23 @@
 import type { IProgramaAcademicoRepositorio } from '../../../dominio/interfaces/repositorio/IProgramaAcademicoRepositorio.js';
 
+
+import { ErrorNoEncontrado } from '../../../errores/errorAplicacion.js';
+import { ErrorValidacion } from '../../../errores/errorAplicacion.js';
+
 export class EliminarProgramaAcademicoUseCase {
   constructor(private readonly programaRepository: IProgramaAcademicoRepositorio) {}
 
   async execute(id: string): Promise<void> {
     if (!id || id.trim().length === 0) {
-      throw new Error('El ID del programa académico es obligatorio.');
-    }
+      throw new ErrorValidacion('El ID del programa academico es obligatorio.');
+    };
 
     const programaExistente = await this.programaRepository.obtenerPorId(id);
     if (!programaExistente) {
-      throw new Error('Programa académico no encontrado.');
-    }
+      throw new ErrorNoEncontrado('Programa academico no encontrado.');
+    };
 
     await this.programaRepository.eliminar(id);
-  }
-}
+  };
+};
 
