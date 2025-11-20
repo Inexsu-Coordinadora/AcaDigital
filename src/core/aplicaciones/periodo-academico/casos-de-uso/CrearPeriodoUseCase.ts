46dec1c9700e78b4bf79b7f19625d0bb50298f7e
Deilons <apolo4748@gmail.com>
2025-11-19 22:42:48 -0500

    feat: implementacion de errores en casos de uso de periodo

--- src/core/aplicaciones/periodo-academico/casos-de-uso/CrearPeriodoUseCase.ts
+++ src/core/aplicaciones/periodo-academico/casos-de-uso/CrearPeriodoUseCase.ts
@@ -3,6 +3,10 @@ import type { IPeriodoAcademico } from '../../../dominio/interfaces/IPeriodoAcad
 import type { IPeriodoRepositorio } from '../../../dominio/interfaces/repositorio/IPeriodoAcademicoRepositorio.js';
 import type { CrearPeriodoDTO } from '../dtos/CrearPeriodoDTO.js';
 
+import { ErrorConflicto } from '../../../errores/errorAplicacion.js';
+import { ErrorReglaNegocio } from '../../../errores/errorAplicacion.js';
+
+
 export class CrearPeriodoUseCase {
     constructor(private repo: IPeriodoRepositorio) { };
 
@@ -12,12 +16,12 @@ export class CrearPeriodoUseCase {
         const fin = new Date(fechaFin);
 
         if (fin <= inicio) {
-            throw new Error('fechaFin debe ser posterior a fechaInicio');
+            throw new ErrorReglaNegocio ('fechaFin debe ser posterior a fechaInicio');
         };
 
         const existe = await this.repo.obtenerPorNombre(nombre);
         if (existe) {
-            throw new Error('Ya existe un periodo con ese nombre');
+            throw new ErrorConflicto ('Ya existe un periodo con ese nombre');
         };
         const periodoEntidad = new PeriodoAcademico({
 
