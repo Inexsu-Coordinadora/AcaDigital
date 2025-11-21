da6d54b81489ecc024e70a0aff6d2739e74e11a0
Deilons <apolo4748@gmail.com>
2025-11-21 09:52:38 -0500

    feat: test de exito completado, ademas se creo la configuracion para ejecutar los test

---
+++ test/unitarios/DefinirPlanEstudioUseCase.test.ts
@@ -0,0 +1,61 @@
+import { DefinirPlanEstudioUseCase } from '../../src/core/aplicaciones/plan-estudio/casos-de-uso/DefinirPlanEstudioUseCase.js';
+import { IProgramaAcademicoRepositorio } from '../../src/core/dominio/interfaces//repositorio/IProgramaAcademicoRepositorio.js';
+import { IAsignaturaRepositorio } from '../../src/core/dominio/interfaces/repositorio/IAsignaturaRepositorio.js';
+import { IPlanEstudioRepositorio } from '../../src/core/dominio/interfaces/repositorio/IPlanEstudioRepositorio.js';
+
+const mockProgramaRepository: Partial<IProgramaAcademicoRepositorio> = {
+    obtenerPorId: jest.fn() as jest.Mock<Promise<any | null>, [string]>,
+
+};
+
+const mockAsignaturaRepository: Partial<IAsignaturaRepositorio> = {
+    obtenerPorId: jest.fn() as jest.Mock<Promise<any | null>, [number]>,
+};
+
+const mockPlanEstudioRepository: Partial<IPlanEstudioRepositorio> = {
+    existeVinculo: jest.fn() as jest.Mock<Promise<boolean>, [string, number]>,
+    guardar: jest.fn() as jest.Mock<Promise<any>, [any]>,
+};
+
+const mockPrograma = { id: 1, nombre: 'Ingenieria de Software' };
+const mockAsignatura = { id: 101, nombre: 'Estructura de Datos' };
+
+const definirPlanEstudioUseCase = new DefinirPlanEstudioUseCase(
+    mockPlanEstudioRepository as IPlanEstudioRepositorio,
+    mockProgramaRepository as IProgramaAcademicoRepositorio,
+    mockAsignaturaRepository as IAsignaturaRepositorio,
+);
+
+const dtoValido = {
+    programaId: 'prog-1',
+    asignaturaId: 101,
+    semestreNivel: 3,
+    creditosCarga: 4,
+};
+
+describe('DefinirPlanEstudioUseCase', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        (mockProgramaRepository.obtenerPorId as jest.Mock).mockResolvedValue(mockPrograma);
+        (mockAsignaturaRepository.obtenerPorId as jest.Mock).mockResolvedValue(mockAsignatura);
+        (mockPlanEstudioRepository.existeVinculo as jest.Mock).mockResolvedValue(false);
+        (mockPlanEstudioRepository.guardar as jest.Mock).mockResolvedValue({
+            id: 'plan-123',
+            ...dtoValido,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        });
+    });
+
+    // Test 1: Caso de exito
+    it('deberia definir exitosamente un nuevo plan de estudio', async () => {
+
+        const resultado = await definirPlanEstudioUseCase.ejecutar(dtoValido);
+
+        expect(mockProgramaRepository.obtenerPorId).toHaveBeenCalledWith('prog-1');
+        expect(mockPlanEstudioRepository.guardar).toHaveBeenCalledTimes(1);
+        expect(resultado).toHaveProperty('id');
+    });
+});
