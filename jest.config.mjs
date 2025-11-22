250fdc5addfc179deaf771a42a6308c5bc92e484
Luis <luis.londo.g@gmail.com>
2025-11-22 00:42:15 -0500

    feature prueba unitaria periodo academico

---
+++ jest.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('jest').Config} */
+const config = {
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+
+  preset: "ts-jest",
+
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^(.*)\\.js$": "$1",
+  },
+
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+
+  moduleFileExtensions: ["js", "ts"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+};
+
+export default config;
